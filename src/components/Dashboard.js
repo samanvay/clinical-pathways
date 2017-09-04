be5565eb074400769f3b34cd987d916a0afd7455
rsatishm <rsatish.m@gmail.com>
2017-09-04 09:52:48 +0530

    added the main header

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,23 +3,29 @@ import {Link} from 'react-router-dom';
 import {Col, Grid, Jumbotron, Row} from "react-bootstrap";
 
 const Dashboard = () =>
-    <Grid bsClass="text-center">
-        <Jumbotron>
+    <div>
+        <div className="jumbotron">
+            <div className="container">
+                <h1>Open Community Health System Admin</h1>
+                <p>Configure Modules. Add Catchment data. Manage Forms/Metadata/Users. </p>
+            </div>
+        </div>
+        <Grid>
             <Row>
                 <Col sm={4}>
                     <Link to="forms">
-                        <i className="fa fa-wpforms fa-5x"></i>
+                        <h3><i className="fa fa-wpforms fa-3x"></i> Forms</h3>
                     </Link>
-                    <h4>Forms</h4>
+                    <p>Build your own forms</p>
                 </Col>
                 <Col sm={4}>
                     <Link to="concepts">
-                        <i className="fa fa-question-circle fa-5x"></i>
+                        <h3><i className="fa fa-question-circle fa-3x"></i> Concepts</h3>
                     </Link>
-                    <h4>Concepts</h4>
+                    <p>Create new concepts</p>
                 </Col>
             </Row>
-        </Jumbotron>
-    </Grid>;
+        </Grid>
+    </div>;
 
 export default Dashboard;
