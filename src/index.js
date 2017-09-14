10b2e59f4b665f80fc3e0bc065a65b9d724eab60
rsatishm <rsatish.m@gmail.com>
2017-09-14 18:04:47 +0530

    Refactoring - restructuring of code, more components

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ import {Provider} from 'react-redux';
 import {createHashHistory} from 'history';
 import {createStore, combineReducers} from 'redux';
 import Reducers from "./reducers/index";
-import Dashboard from "./components/Dashboard";
 
 import 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
-import Forms from "./components/Forms";
 import AdminHeader from "./components/AdminHeader";
 import Breadcrumb from "./components/Breadcrumb";
-import App from "./App";
 import Routes from "./routes";
 
 const history = createHashHistory({queryKey: false})
